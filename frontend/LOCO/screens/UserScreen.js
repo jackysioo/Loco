9bd9f73e1ded5a0aee99113258ed1cfb84d54abb
lisaechen <lisaechen@gmail.com>
2019-10-20 13:14:50 -0700

    Edited User Profile

--- frontend/LOCO/screens/UserScreen.js
+++ frontend/LOCO/screens/UserScreen.js
@@ -29,51 +29,74 @@ class UserScreen extends React.Component {
                         style={{ width, marginTop: '10%' }}>
                         <View style={styles.profileCard}>
                             <View style={styles.profilePicContainer}>
-                                <Image
-                                    source={{ uri: Images.ProfilePicture }}
-                                    style={styles.profilePic}/>
+                                <View>
+                                    <Text
+                                        style={{
+                                            fontSize: 30,
+                                            textShadowColor: "#895da3",
+                                            color: "#ffffff",
+                                            textShadowOffset: { width: -1, height: 0 },
+                                            textShadowRadius: 10,
+                                            marginBottom: 10,
+                                            fontWeight: "400",
+                                            textAlign: "center"
+                                        }}>
+                                        UserName123
+                                    </Text>
+                                    <Image source={{ uri: Images.ProfilePicture }} style={styles.profilePic} />
+
+                                </View>
+
+                                <View style={styles.profileButtons}>
+                                    <Button
+                                        title="Edit Profile"
+                                        color="#4c1287">
+                                    </Button>
+                                </View>
                             </View>
                         </View>
-                        <View style={styles.info}>
-                            <View style={styles.profileButtons}>
-                                <Button
-                                    title="Follow"
-                                    color="#ffffff"
-                                    backgroundColor="#51bfbb">
-                                </Button>
-                                <Button
-                                    title="Message"
-                                    color="#ffffff"
-                                    backgroundColor="#0e4f64">
-                                </Button>
+                        <View style={styles.userStatOuter}>
+                            <View style={styles.userStatInner}>
+                                <Text style={styles.userStatsNum}>2000</Text>
+                                <Text style={styles.userStats}>Followers</Text>
                             </View>
-                            <View style={styles.profileButtons}>
-                                    <Text
-                                        bold
-                                        size={12}
-                                        color={Colors.text}
-                                        style={{ marginBottom: 4 }}>
-                                        2K
-                                    </Text>
-                                    <Text size={12}>Followers</Text>
-                                    <Text
-                                        bold
-                                        color={Colors.text}
-                                        size={12}
-                                        style={{ marginBottom: 4 }}>
-                                        10
-                                    </Text>
-                                    <Text size={12}>Reviews</Text>
-                                    <Text
-                                        bold
-                                        color={Colors.text}
-                                        size={12}
-                                        style={{ marginBottom: 4 }}>
-                                        5
-                                    </Text>
-                                    <Text size={12}>Photos</Text>
+                            <View style={styles.userStatInner}>
+                                <Text style={styles.userStatsNum}>10</Text>
+                                <Text style={styles.userStats}>Reviews</Text>
+                            </View>
+                            <View style={styles.userStatInner}>
+                                <Text style={styles.userStatsNum}>5</Text>
+                                <Text style={styles.userStats}>Photos</Text>
                             </View>
                         </View>
+
+                        <View style={styles.personalInfo}>
+                            <View style={{ marginTop: 20 }}>
+                                <Text style={styles.title}>Your Bio: </Text>
+                                <Text style={styles.text}>This is where the user writes their bio. Interests, hobbies, skills, etc. They can also write about their experiences and different certificates or other form of validations for the skills they have.</Text>
+                            </View>
+                            <View style={{ borderBottomColor: "#9587a3", borderBottomWidth: 1.3, marginTop: 10, }}/>
+                            <View style={styles.nameAge}>
+                                <Text>
+                                    <Text style={styles.title}>Full Name: </Text>
+                                    <Text style={styles.text}>FirstName LastName</Text>
+                                </Text>
+                            </View>
+                            <View style={{ flexDirection: "column", marginTop: 10 }}>
+                                <Text style={styles.title}>Address: </Text>
+                                <Text style={styles.text}>8888 Address Street</Text>
+                                <Text style={styles.text}>Vancouver, BC</Text>
+                                <Text style={styles.text}>A8A 8A8</Text>
+                            </View>
+                            <View style={styles.nameAge}>
+                                <Text>
+                                    <Text style={styles.title}>Age: </Text>
+                                    <Text style={styles.text}>20</Text>
+                                </Text>
+                            </View>
+                        </View>
+
+                                        
                     </ScrollView>
                 </ImageBackground>
             </View>
@@ -95,7 +118,7 @@ const styles = StyleSheet.create({
     },
     profileBackground: {
         width: width,
-        height: height / 2
+        height: height
     },
     profilePicContainer: {
         position: "relative",
@@ -107,8 +130,6 @@ const styles = StyleSheet.create({
         marginTop: 65,
         borderTopLeftRadius: 6,
         borderTopRightRadius: 6,
-        backgroundColor: '#ffffff',
-        shadowColor: '#000000',
         shadowOffset: { width: 0, height: 0 },
         shadowRadius: 8,
         shadowOpacity: 0.2,
@@ -121,22 +142,63 @@ const styles = StyleSheet.create({
         borderWidth: 0,
         alignSelf: "center"
     },
-    info: {
-        paddingHorizontal: 40
-    },
-    profileButtons: { 
-        marginTop: 20, 
-        paddingBottom: 24, 
+    profileButtons: {
+        marginTop: 1,
+        paddingBottom: 5,
         alignSelf: "center",
-        flexDirection: 'row' 
+        flexDirection: 'row',
+    },
+    userStatOuter: {
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: 'row',
+        paddingVertical: 5,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    },
+    userStatInner: {
+        flexDirection: "column",
+        paddingHorizontal: 25,
+    },
+    userStats: {
+        textAlign: "center",
+        fontSize: 17,
+        color: "#4c1287",
+        fontWeight: '300',
+    },
+    userStatsNum: {
+        textAlign: "center",
+        fontSize: 20,
+        color: "#4c1287",
+        fontWeight: '300',
+        marginRight: 5,
     },
     thumb: {
-      borderRadius: 4,
-      marginVertical: 4,
-      alignSelf: "center",
-      width: thumbMeasure,
-      height: thumbMeasure
-    }
+        borderRadius: 4,
+        marginVertical: 4,
+        alignSelf: "center",
+        width: thumbMeasure,
+        height: thumbMeasure
+    },
+    nameAge: {
+        flexDirection: "row",
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    personalInfo: {
+        flexDirection: "column",
+        marginHorizontal: 20,
+    },
+    text: {
+        textAlign: "center",
+        color: "#4c1287",
+        fontWeight: "300",
+        fontSize: 17,
+    },
+    title: {
+        color: "#4c1287",
+        fontWeight: "300",
+        fontSize: 20,
+    },
 });
 
 export default UserScreen;
