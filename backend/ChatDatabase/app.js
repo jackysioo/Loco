da9afc80db00514ae498816ccc3a320132a04cd4
jackysioo <jackysio.24@gmail.com>
2019-10-27 16:59:43 -0700

    changes to app.js for chat

--- backend/ChatDatabase/app.js
+++ backend/ChatDatabase/app.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io');
+// const express = require('express');
+// const app = express();
+// const http = require('http').Server(app);
+// const socketIo = require('socket.io');
 
+// const port = process.env.PORT || 1337;
+// const index = require("./routes/index"); //change this
+// app.use(index);
+// const server = http.createServer(app);
+// const io = socketIo(server); // < Interesting!
+
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+//const axios = require("axios");
 const port = process.env.PORT || 1337;
-const index = require("./routes/index"); //change this
+const index = require("./routes/index");
 const app = express();
 app.use(index);
 const server = http.createServer(app);
 const io = socketIo(server); // < Interesting!
+
 
 io.on("connection", socket => {
     console.log("New client connected");
@@ -17,6 +28,9 @@ io.on("connection", socket => {
   });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
 
 
 // //database connection
