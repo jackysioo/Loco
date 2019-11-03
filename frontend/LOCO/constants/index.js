aae5837e6b264b28b296c69d51c1cbe3bdd0a502
Cynzy <cynthiazhou98@gmail.com>
2019-11-03 14:39:37 -0800

    deleted tabs file

--- frontend/LOCO/constants/index.js
+++ frontend/LOCO/constants/index.js
@@ -1,6 +1,5 @@
 import Images from './Images';
 import Colors from './Colors';
-import tabs from './tabs';
 import businesses from './businesses';
 import user from './user';
 import SortBy from './SortBy'
