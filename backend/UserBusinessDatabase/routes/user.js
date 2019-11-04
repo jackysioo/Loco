d417bae4b94ed499d53aeefd06eb4f15feeaa015
Cynthia <cynthiazhou98@gmail.com>
2019-11-04 10:10:11 -0800

    changed to caps

--- backend/UserBusinessDatabase/routes/user.js
+++ backend/UserBusinessDatabase/routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express'); 
-const router = express.Router();  
+const Router = express.Router();  
 const userController = require('../controllers/user');
 
-router.get('/get', userController.getUserData); 
-router.post('/post',userController.postUserData);
-router.get('/get/:userId',userController.getUserDataById); 
-router.put('/put/:userId',userController.updateUserData);  
-router.put('/putReviewService/:userId',userController.updateReviewService);
-router.delete('/delete/:userId',userController.deleteUser); 
+Router.get('/get', userController.getUserData); 
+Router.post('/post',userController.postUserData);
+Router.get('/get/:userId',userController.getUserDataById); 
+Router.put('/put/:userId',userController.updateUserData);  
+Router.put('/putReviewService/:userId',userController.updateReviewService);
+Router.delete('/delete/:userId',userController.deleteUser); 
 
-module.exports = router;
+module.exports = Router;
