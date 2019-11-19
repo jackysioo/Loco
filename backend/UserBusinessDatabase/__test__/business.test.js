c0ed4c6fa04e0cf9f956ed95f17332262caab3c9
Alfred Chang <alfredchang0409@gmail.com>
2019-11-19 13:54:06 -0800

    small changes to simplify search

--- backend/UserBusinessDatabase/__test__/business.test.js
+++ backend/UserBusinessDatabase/__test__/business.test.js
@@ -5,7 +5,23 @@ const supertest = require('supertest');
 const request = supertest(app); 
 const mongoose = require('mongoose');
 
-const dbHandler = require('./db');
+const dbHandler = require('./db'); 
+
+const userData = [
+  {
+      username: "tanya_cooper123",
+      firstName: "Tanya",
+      lastName: "Cooper",
+      profilePic: 'https://images.unsplash.com/photo-1481824429379-07aa5e5b0739?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=942&q=80',
+      addressLine: "8888 Address Street",
+      addressCity: "Vancouver",
+      addressProvince: "BC",
+      addressPostalCode: "A8A 8A8",
+      birthday: "Feb 14, 1996",
+      phoneNumber: "604-888-8888",
+      email: "tanya_cooper@gmail.com", 
+  },
+]; 
 
 const businessData = [
     {
@@ -25,7 +41,74 @@ const businessData = [
       },
       price: "$15/set",
       tags: ["language", "korean", "japanese"]
-    },
+    }, 
+    {
+      title: 'Calculus I & II Tutor',
+      user: 'Carly',
+      about: 'Arcu cursus euismod quis viverra nibh cras. Nulla pellentesque dignissim enim sit amet venenatis urna cursus eget. Scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt. Fermentum iaculis eu non diam phasellus.',
+      profilePic: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+      images: [
+        'https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&q=80',
+        'https://images.unsplash.com/photo-1560785477-d43d2b34e0df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80'
+      ],
+      rating: 4.4,
+      price: '$35/hr',
+      region: 'Kitsilano',
+      location: {
+        lat: 49.2684,
+        'long': -123.1683
+      },
+      tags: [
+        'tutor',
+        'math',
+        'calculus'
+      ]
+    }, 
+    {
+      title: '3D Volume Lash Extensions',
+      user: 'Sasha',
+      about: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      profilePic: 'https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80',
+      images: [
+        'https://images.unsplash.com/photo-1493422884938-abd42cfa0f29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1024&q=80',
+        'https://images.unsplash.com/photo-1560785477-d43d2b34e0df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80'
+      ],
+      rating: 3.89,
+      price: '$80-$120',
+      region: 'Downtown Vancouver',
+      location: {
+        lat: 49.282,
+        'long': -123.1171
+      },
+      tags: [
+        'beauty',
+        'lashes',
+        'spa'
+      ]
+    }, 
+    {
+      title: 'HomemadeNailFungusSalads',
+      user: 'SaladQueen',
+      about: 'eat my salad or ill hunt u down',
+      profilePic: 'https://i.pinimg.com/originals/c2/34/99/c2349994d18f254db5e6ddee44a90348.jpg',
+      images: [
+        'http://blogs.newcastle.edu.au/wp-content/uploads/2012/12/salad-shoe.jpg',
+        'https://media-cdn.tripadvisor.com/media/photo-s/0f/22/c6/82/disgusting-salad.jpg'
+      ],
+      rating: 0.01,
+      region: 'West Vancouver',
+      location: {
+        lat: 49.26,
+        'long': -123.24
+      },
+      price: '$45/salad',
+      tags: [
+        'food',
+        'homecooked',
+        'fungus',
+        'salad'
+      ]
+    }
   ]; 
 
   beforeAll(async () => {await dbHandler.connect(); });
