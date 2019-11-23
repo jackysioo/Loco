e7ec84af80ca9afb30038b0475fcbc9cd06e239e
jacky sio <jackysio.24@gmail.com>
2019-11-23 15:53:01 -0800

    fixing some bugs

--- backend/UserBusinessDatabase/recEngine/engine.js
+++ backend/UserBusinessDatabase/recEngine/engine.js
@@ -9,8 +9,8 @@ const Suggestion = require('./Suggestion');
 
 module.exports = class Engine {
     constructor() {
-        this.likes = new Rater(this,'likes'), 
-        this.dislikes = new Rater(this,'dislikes'), 
+        this.likes = new Rater(this,'likes'); 
+        this.dislikes = new Rater(this,'dislikes'); 
         this.similars = new Similars(this); 
         this.suggestions = new Suggestion(this);
     } 
