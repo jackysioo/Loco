d58d87d7327002c563533fb743a5a3ea48199060
jacky sio <jackysio.24@gmail.com>
2019-11-23 15:59:26 -0800

    review and added some comments

--- backend/UserBusinessDatabase/recEngine/suggestion.js
+++ backend/UserBusinessDatabase/recEngine/suggestion.js
@@ -9,28 +9,41 @@ module.exports = class Suggestion {
     } 
 
     async update(userId){  
+        //find one similar by User id 
         const others = await engine.similar.byUser(userId); 
+        //find all items the User likes 
         const userLikes = await engine.likes.itemsByUser(userId);
+        //find all items the User dislikes
         const userDislikes = await engine.dislikes.itemsByUser(userId);
+        //for each similar 
 
-        const items = others.map((other) => { 
+         
+        const items = others.similarity.map((other) => { 
            return [engine.likes,engine.dislikes].map(rater => { 
                return rater.itemsByUser(other);
             });
         }); 
 
+        //find all the unique items the user has not rated yet
         items= _.difference(_.unique(_.flatten(items)),userLikes,userDislikes); 
         const suggestions = items.map( async (item) => { 
+
+            //find all the users that like and dislike this item
             const likers = await this.engine.likes.usersByItem(item); 
             const dislikers =await this.engine.dislikes.usersByItem(item); 
-            var numerator = 0; 
+
+            var numerator = 0;
+            //ref is an array of other users that rated this item, exculding the user 
             var ref = _.without(_.flatten([likers,dislikers]),userId);  
             var len;
+
             for(var i = 0, len = ref.length; i < len; i++){  
                 var other = ref[i]; 
-                other = _.findWhere(others, { 
+                other = _.findWhere(others.similarity, { 
                     user: other
                 }); 
+
+                // need to check if like or dislike
                 if(other != null){ 
                     numerator += other.similarity;
                 }
@@ -38,7 +51,7 @@ module.exports = class Suggestion {
             } 
             return {item: item, weight: numerator / _.union(likers,dislikers).length};
         }); 
-
+ 
         const result = new Suggestion({user: userId, suggestions: suggestions}); 
         result.save();
 
