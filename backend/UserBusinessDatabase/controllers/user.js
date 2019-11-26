2e6b1f7e2513adfcba18b320f55f902fb5f7b77b
Alfred Chang <alfredchang0409@gmail.com>
2019-11-26 00:06:07 -0800

    signin

--- backend/UserBusinessDatabase/controllers/user.js
+++ backend/UserBusinessDatabase/controllers/user.js
@@ -20,7 +20,23 @@ createToken = user => {
 
 exports.signIn = async (req,res,next) => { 
     const token = createToken(req.user); 
-    res.status(200).json({token});
+    User.findOne({username: req.body.username}) 
+    .populate('reviews services') 
+    .exec()
+    .then((user) => {
+        if (!user) {
+            const error = new Error('Could not find user');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({ user: user,token: token })
+    })
+    .catch((err) => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    })
 
 }
 
