6a26c668e7bf2b0ab243de832dd7357733b9b110
lisaechen <lisaechen@gmail.com>
2019-11-26 05:31:30 -0800

    Update

--- frontend/LOCO/specs/CreateUserSpec.js
+++ frontend/LOCO/specs/CreateUserSpec.js
@@ -3,18 +3,17 @@ export default function (spec) {
     spec.describe('Creating User', function () {
         spec.it('W O R K S', async function () {
             await spec.press('SignUp.Button');
-            await spec.fillIn('Username.TextInput', 'lisachen123');
-            await spec.fillIn('Password.TextInput', 'thisismypassword');
+            await spec.fillIn('Username.TextInput', 'LISA');
+            await spec.fillIn('Password.TextInput', '0');
             await spec.fillIn('FirstName.TextInput', 'Lisa');
             await spec.fillIn('LastName.TextInput', 'Chen');
-            await spec.fillIn('AddressLine.TextInput', '1234 Address Street');
+            await spec.fillIn('AddressLine.TextInput', '6488 University Blvd,');
             await spec.fillIn('City.TextInput', 'Vancouver');
             await spec.fillIn('Province.TextInput', 'BC');
-            await spec.fillIn('PostalCode.TextInput', 'A8A 8A8');
-            await spec.fillIn('Phone.TextInput', '888-888-8888');
+            await spec.fillIn('PostalCode.TextInput', 'V6T 1Z4');
+            await spec.fillIn('Phone.TextInput', '(604) 822-2811');
             await spec.fillIn('Email.TextInput', 'email123@hotmail.com');
-            await spec.fillIn('AboutMe.TextInput', 'Hi my name is Lisa and I like penguins');
-            await spec.press('SaveChanges.Button.Button');
+            await spec.fillIn('AboutMe.TextInput', 'Hi my name is Lisa\nI like to bake in my free time');
         });
     });
 }
