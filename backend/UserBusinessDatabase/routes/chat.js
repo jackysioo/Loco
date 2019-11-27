9c90b8e2e5b7ea0f0d7b07b257324b28f122a5a5
Alfred Chang <alfredchang0409@gmail.com>
2019-11-26 20:44:05 -0800

    dback and forth

--- backend/UserBusinessDatabase/routes/chat.js
+++ backend/UserBusinessDatabase/routes/chat.js
@@ -1,14 +1,16 @@
 const express = require('express'); 
 const Router = express.Router();  
-const chatController = require('../controllers/chat');
+const chatController = require('../controllers/chat'); 
+const passport = require('passport');
+const passportConf = require('../passport');
 
-Router.get('/chats',chatController.getChats);
-Router.get('/users',chatController.getUser); 
-Router.get('/users',chatController.getUsers); 
-Router.post('/users',chatController.postUser); 
-Router.get('/messages',chatController.getMessages); 
-Router.post('/room',chatController.postRoom);  
-Router.post('/messages',chatController.postMessages);  
-Router.delete('/room',chatController.deleteRoom); 
+Router.get('/chats',passport.authenticate('jwt',{session: false}),chatController.getChats);
+Router.get('/users',passport.authenticate('jwt',{session: false}),chatController.getUser); 
+Router.get('/users',passport.authenticate('jwt',{session: false}),chatController.getUsers); 
+Router.post('/users',passport.authenticate('jwt',{session: false}),chatController.postUser); 
+Router.get('/messages',passport.authenticate('jwt',{session: false}),chatController.getMessages); 
+Router.post('/room',passport.authenticate('jwt',{session: false}),chatController.postRoom);  
+Router.post('/messages',passport.authenticate('jwt',{session: false}),chatController.postMessages);  
+Router.delete('/room',passport.authenticate('jwt',{session: false}),chatController.deleteRoom); 
 
 module.exports = Router;
